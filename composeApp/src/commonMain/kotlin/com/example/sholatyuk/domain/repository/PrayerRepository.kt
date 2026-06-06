32a6e31238b8a45b44ce9faa6d24e19cd0f6a6f9
15-187-gilang <gilangsuryaagung25@gmail.com>
2026-06-06 17:43:58 +0700

    feat: implementasi operasi CRUD untuk data jadwal shalat

--- composeApp/src/commonMain/kotlin/com/example/sholatyuk/domain/repository/PrayerRepository.kt
+++ composeApp/src/commonMain/kotlin/com/example/sholatyuk/domain/repository/PrayerRepository.kt
@@ -1,2 +1,13 @@
 package com.example.sholatyuk.domain.repository
 
+import com.example.sholatyuk.domain.model.PrayerTime
+import kotlinx.coroutines.flow.Flow
+import kotlinx.datetime.LocalDate
+
+interface PrayerRepository {
+    fun getPrayerTimeByDate(date: LocalDate): Flow<PrayerTime?>
+    suspend fun fetchAndSavePrayerTime(latitude: Double, longitude: Double, date: LocalDate): Result<PrayerTime>
+    suspend fun savePrayerTime(prayerTime: PrayerTime)
+    suspend fun updatePrayerTime(prayerTime: PrayerTime)
+    suspend fun deletePrayerTime(date: LocalDate)
+}
